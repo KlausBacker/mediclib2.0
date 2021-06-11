9a4cfeb5ea2fd24006435c413427914a39353915
Nicolas Boulanger <boulanger.ncls@gmail.com>
2021-06-11 09:11:43 +0200

    init

---
+++ sb_mediclib/src/main/java/com/mediclib/sb_mediclib/SbMediclibApplication.java
@@ -0,0 +1,13 @@
+package com.mediclib.sb_mediclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMediclibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMediclibApplication.class, args);
+    }
+
+}
---
+++ sb_mediclib/src/test/java/com/mediclib/sb_mediclib/SbMediclibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediclib.sb_mediclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMediclibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
